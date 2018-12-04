f23826e99cff6bbe5d202d951450164ca1c4339e
Christian Griffin <c.griffin@vehikl.com>
2018-12-04 11:37:42 -0500

    Day 2, star 2 completed.

---
+++ day_2/star_2.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../helpers.php';
+
+$boxes = loadFile(__DIR__ . '/boxes.txt');
+
+$similarBoxes = [];
+foreach($boxes as $box) {
+    foreach($boxes as $innerBox) {
+        if(levenshtein($box, $innerBox) === 1) {
+            $similarBoxes[$box] = null;
+            $similarBoxes[$innerBox] = null;
+        }
+    }
+}
+
+var_dump($similarBoxes);
+
+$similarBoxes = array_map(
+    function ($value) {
+        return str_split($value);
+    },
+    array_keys($similarBoxes)
+);
+
+$similarBoxes = implode(array_intersect_assoc(...$similarBoxes));
+
+echo "$similarBoxes\n";
+exit;
